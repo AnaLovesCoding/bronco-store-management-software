1f3f833dd4fa7f5203f98619103859472ba4e289
ortizalec <ortizalec@icloud.com>
2022-11-18 15:45:09 -0800

    mvn -B archetype:generate ...

---
+++ bronco-store-management/src/main/java/cpp/cs5800/app/App.java
@@ -0,0 +1,13 @@
+package cpp.cs5800.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
