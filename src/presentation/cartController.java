d29ec01b49ede31fe8b8e5cc31b8f9154d07b12f
Archana G V <gvarchana95@gmail.com>
2022-11-24 15:37:20 -0800

    UI of Shop page, View Cart,Receipt,Order Status

--- src/presentation/cartController.java
+++ src/presentation/cartController.java
@@ -6,43 +6,41 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class welcomeCustomerController {
+public class cartController {
     private Stage stage;
     private Scene scene;
     private Parent root;
-
     @FXML
-    protected void onPlaceOrderClick(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("shop.fxml"));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    @FXML
-    protected void onLogoutClick(ActionEvent event) throws IOException {
+    protected void onLogoutClick(ActionEvent event)throws IOException {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("bsm_home.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-
     @FXML
-    protected void onCheckoutClick(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("order_status.fxml"));
+    protected void onShopClick(ActionEvent event)throws IOException {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("shop.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-}
+    @FXML
+    protected void onSubmitPaymentClick(ActionEvent event)throws IOException {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("receipt.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+
+
+    }
 
---
+++ src/presentation/shopController.java
@@ -0,0 +1,37 @@
+package presentation;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class shopController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    protected void onLogoutClick(ActionEvent event)throws IOException {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("bsm_home.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    protected void switchToViewCart(ActionEvent event)throws IOException {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("cart.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    }
+
