3008c4d38b5950d9295f950678ad1e42b076a6ac
Archana G V <gvarchana95@gmail.com>
2022-12-05 02:18:09 -0800

    Receipt Page

--- src/presentation/receiptController.java
+++ src/presentation/receiptController.java
@@ -1,19 +1,53 @@
 package presentation;
 
+import data.BsmUserData;
+import data.CartProduct;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class receiptController {
+import static data.CartData.cartProductsList;
+
+public class receiptController implements Initializable {
+    @FXML
+    private TableView receiptTable;
+
+    @FXML
+    private TableColumn productId, productName, price, quantity;
+    @FXML
+    private TextField discount, totalPrice;
     private Stage stage;
     private Scene scene;
     private Parent root;
+    Double totalPriceValue = 0.0;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        receiptTable.getColumns().addAll(productId, productName, quantity, price);
+        productId.setCellValueFactory(new PropertyValueFactory<CartProduct, String>("id"));
+        productName.setCellValueFactory(new PropertyValueFactory<CartProduct, String>("name"));
+        quantity.setCellValueFactory(new PropertyValueFactory<CartProduct, String>("quantity"));
+        price.setCellValueFactory(new PropertyValueFactory<CartProduct, String>("price"));
+
+        receiptTable.setItems(cartProductsList);
+
+        setUpPrice();
+
+    }
+
     @FXML
     protected void onLogoutClick(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("bsm_home.fxml"));
@@ -23,5 +57,19 @@ public class receiptController {
         stage.show();
     }
 
+
+    private void setUpPrice() {
+        discount.setText(BsmUserData.user.getDiscount().toString() + "%");
+        cartProductsList.forEach(cartProduct -> {
+            totalPriceValue = totalPriceValue + cartProduct.getPrice();
+        });
+
+        double discountValue = Double.parseDouble(BsmUserData.user.getDiscount().toString());
+        totalPriceValue = totalPriceValue - (
+                totalPriceValue * (discountValue / 100)
+        );
+
+        totalPrice.setText(totalPriceValue.toString());
     }
+}
 
